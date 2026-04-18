3221c85f5f7ed9c6e0570546d865505c8df2c0c5
Ivan Boichuk <ivan.boychuck@gmail.com>
2026-04-18 13:03:28 +0200

    feat: add product order options component and related functionality

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,7 @@ import setScrollBarWidth from 'set-scrollbar-width';
 import './video-player';
 import './swiper-init';
 import './lightgallery-init';
+import './product-order';
 import { initMenu } from './menu';
 import { initCounterAnimation } from './counter-animation';
 
