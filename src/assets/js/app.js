0c5212a996a19abea3997a7cdc12d17996ece76f
Ivan Boichuk <ivan.boychuck@gmail.com>
2026-04-18 12:24:19 +0200

    feat: implement product gallery with lightbox and variations

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 import setScrollBarWidth from 'set-scrollbar-width';
 import './video-player';
 import './swiper-init';
+import './lightgallery-init';
 import { initMenu } from './menu';
 import { initCounterAnimation } from './counter-animation';
 
